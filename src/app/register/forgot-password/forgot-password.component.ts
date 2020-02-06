44075e9130191b5e421e19df3e90e562d0ceb7bf
haonguyen <haonguyen@tamdongtam.vnnode>
2020-02-06 14:44:24 +0700

    create forgot password screen

--- src/app/register/forgot-password/forgot-password.component.ts
+++ src/app/register/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { requiredInput } from 'src/app/core/helper/custom-validate.helper';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  // @ViewChild('email', { static: false }) email: ElementRef;
+  // @ViewChild('dateForm', { static: false }) dateForm: ElementRef;
+  forgotPasswordForm: FormGroup;
+  isSubmitted: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.initForgotPasswordForm();
+  }
+  initForgotPasswordForm() {
+    this.forgotPasswordForm = new FormGroup({
+        email: new FormControl('', requiredInput),
+        dateInput: new FormControl('', requiredInput),
+    });
+  }
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.forgotPasswordForm.invalid) {
+        return;
+    }
   }
 }
