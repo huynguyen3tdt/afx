32e41779cf42c2fb2fb937ef9bf2c0e774c78c64
haonguyen <haonguyen@tamdongtam.vnnode>
2020-02-12 17:48:56 +0700

    update

--- src/app/manage/account-information/account-information.component.ts
+++ src/app/manage/account-information/account-information.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { WithdrawRequestService } from 'src/app/core/services/withdraw-request.service';
 
 @Component({
   selector: 'app-account-information',
@@ -7,14 +8,25 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./account-information.component.css']
 })
 export class AccountInformationComponent implements OnInit {
+  public listAccountInfor;
 
-  constructor(private translateee: TranslateService) { }
+  constructor(private translateee: TranslateService,
+              private withdrawRequestService: WithdrawRequestService) { }
 
   ngOnInit() {
+    this.getMt5Infor();
   }
 
   changeLang(event) {
     this.translateee.use(event);
   }
 
+  getMt5Infor() {
+    this.withdrawRequestService.getmt5Infor().subscribe(response => {
+      if (response.status === 200) {
+        this.listAccountInfor = response.body.data;
+
+      }
+    });
+  }
 }
