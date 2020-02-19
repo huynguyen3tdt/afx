b1055636bf6e5cc01bed6be1798c858cd266fd53
Nguyễn Minh Hiếu <hieunguyen@tamdongtam.vn>
2020-02-19 11:37:51 +0700

    update for deposit tab

--- src/app/manage/deposit/deposit.component.ts
+++ src/app/manage/deposit/deposit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { requiredInput } from 'src/app/core/helper/custom-validate.helper';
+declare var $: any;
 
 @Component({
   selector: 'app-deposit',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deposit.component.css']
 })
 export class DepositComponent implements OnInit {
-
+  depositAmountForm: FormGroup;
+  depositTransactionForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.showInforBank('ufj_bank');
+    this.initDepositAmountForm();
+    this.initDepositTransactionForm();
+  }
+
+  initDepositAmountForm() {
+    this.depositAmountForm = new FormGroup ({
+      deposit: new FormControl('', requiredInput)
+    });
+  }
+
+  initDepositTransactionForm() {
+    this.depositTransactionForm = new FormGroup({
+      deposit: new FormControl('', requiredInput)
+    });
+  }
+
+  showInforBank(bank: string) {
+    const listTab = ['ufj_bank', 'mizuho_bank', 'sm_bank', 'jpb_bank', 'jn_bank', 'rakuten_bank'];
+    listTab.forEach(element => {
+      if (bank === element) {
+        $(`a#${element}`).addClass('selected');
+        $(`div#${element}`).show();
+      } else {
+        $(`a#${element}`).removeClass('selected');
+        $(`div#${element}`).hide();
+      }
+    });
   }
 
 }
