2bfb10ec7a58087bcadb05ff067185cd534bc070
Nguyễn Minh Hiếu <hieunguyen@tamdongtam.vn>
2020-02-26 15:42:58 +0700

    update API for report screen

--- src/app/manage/report-list/report-list.component.spec.ts
+++ src/app/manage/report-list/report-list.component.spec.ts
@@ -1,9 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReportListComponent } from './report-list.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { PaginationModule } from 'ngx-bootstrap';
+import { PaginationModule, BsDatepickerModule } from 'ngx-bootstrap';
 import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 import { RouterTestingModule } from '@angular/router/testing';
 import { EnvConfigService } from 'src/app/core/services/env-config.service';
@@ -16,11 +16,13 @@ describe('ReportListComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ ReportListComponent ],
       imports: [
+        ReactiveFormsModule,
         FormsModule,
         HttpClientTestingModule,
         PaginationModule.forRoot(),
         Ng4LoadingSpinnerModule,
-        RouterTestingModule
+        RouterTestingModule,
+        BsDatepickerModule.forRoot()
       ],
       providers: [ReportListComponent, EnvConfigService]
     })
--- src/app/manage/report-list/report-list.component.ts
+++ src/app/manage/report-list/report-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ReportService } from 'src/app/core/services/report.service';
 import { ReportIDS } from 'src/app/core/model/report-response.model';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ACCOUNT_TYPE } from 'src/app/core/constant/authen-constant';
 import * as moment from 'moment';
 
 @Component({
@@ -9,61 +11,132 @@ import * as moment from 'moment';
   styleUrls: ['./report-list.component.css']
 })
 export class ReportListComponent implements OnInit {
-
   currentPage: number;
   pageSize: number;
   totalItem: number;
   tab: string;
   listReport: Array<ReportIDS>;
+  searchForm: FormGroup;
+  tradingAccount: number;
+  listTradingAccount: Array<number> = [];
+  listTotalItem: Array<number> = [10, 20, 30];
+  recordFrom: number;
+  recordTo: number;
 
+  TABS = {
+    ALL: { name: 'ALL', value: -1 },
+    DAILY: { name: 'DAILY', value: 0 },
+    YEARLY: { name: 'YEARLY', value: 1 },
+  };
 
+  DURATION = {
+    DAY: 'day',
+    MONTH: 'month',
+    YEAR: 'year'
+  };
 
   constructor(private reportservice: ReportService, ) { }
 
   ngOnInit() {
     this.currentPage = 1;
     this.pageSize = 10;
-    this.getReport(this.currentPage, this.pageSize, -1);
+    this.tradingAccount = Number(localStorage.getItem(ACCOUNT_TYPE));
+    this.listTradingAccount.push(Number(localStorage.getItem(ACCOUNT_TYPE)));
+    this.initSearchForm();
+    this.getReport(this.searchForm.controls.tradingAccount.value, this.currentPage, this.pageSize, this.TABS.ALL.value,
+      this.formatDate(this.searchForm.controls.fromDate.value), this.formatDate(this.searchForm.controls.toDate.value));
   }
-  getReport(pageSize: number, pageNumber: number, type?: number) {
+
+  initSearchForm() {
+    this.searchForm = new FormGroup({
+      tradingAccount: new FormControl(this.listTradingAccount[0]),
+      fromDate: new FormControl(null),
+      toDate: new FormControl(null)
+    });
+  }
+
+  getReport(accountNumber: number, pageSize: number, pageNumber: number, type?: number, dateFrom?: string, dateTo?: string) {
     this.checkTab(type);
-    this.reportservice.getReport(pageSize, pageNumber, type).subscribe(response => {
+    this.reportservice.getReport(accountNumber, pageNumber, pageSize, type, dateFrom, dateTo).subscribe(response => {
       if (response.meta.code === 200) {
         this.listReport = response.data.results;
+        this.totalItem = response.data.count;
         this.listReport.forEach(item => {
           item.created_date = moment(item.created_date).format('YYYY/MM/DD');
         });
+        this.recordFrom = this.pageSize * (this.currentPage - 1) + 1;
+        this.recordTo = this.recordFrom + (this.listReport.length - 1);
       }
     });
   }
 
-  pageChanged(event) {
-    this.currentPage = event.page;
+  searchReport() {
+    if (this.searchForm.controls.fromDate.value !== null && this.searchForm.controls.fromDate.value.toString().indexOf('/') === -1) {
+      this.searchForm.controls.fromDate.setValue(moment(new Date(this.searchForm.controls.fromDate.value)).format('DD/MM/YYYY'));
+    }
+    if (this.searchForm.controls.toDate.value !== null && this.searchForm.controls.toDate.value.toString().indexOf('/') === -1) {
+      this.searchForm.controls.toDate.setValue(moment(new Date(this.searchForm.controls.toDate.value)).format('DD/MM/YYYY'));
+    }
     switch (this.tab) {
-      case 'ALL':
-        this.getReport(this.currentPage, this.pageSize, -1);
+      case this.TABS.ALL.name:
+        this.getReport(this.searchForm.controls.tradingAccount.value, this.currentPage, this.pageSize, this.TABS.ALL.value,
+          this.formatDate(this.searchForm.controls.fromDate.value), this.formatDate(this.searchForm.controls.toDate.value));
         break;
-      case 'TRANSACTION':
-        this.getReport(this.currentPage, this.pageSize, 0);
+      case this.TABS.DAILY.name:
+        this.getReport(this.searchForm.controls.tradingAccount.value, this.currentPage, this.pageSize, this.TABS.DAILY.value,
+          this.formatDate(this.searchForm.controls.fromDate.value), this.formatDate(this.searchForm.controls.toDate.value));
         break;
-      case 'ANNUAL':
+      case this.TABS.YEARLY.name:
+        this.getReport(this.searchForm.controls.tradingAccount.value, this.currentPage, this.pageSize, this.TABS.YEARLY.value,
+          this.formatDate(this.searchForm.controls.fromDate.value), this.formatDate(this.searchForm.controls.toDate.value));
         break;
     }
   }
+
+  pageChanged(event) {
+    this.currentPage = event.page;
+    this.searchReport();
+  }
+
+  changeTotalItem(event) {
+    this.pageSize = event.target.value;
+    this.currentPage = 1;
+    this.searchReport();
+  }
+
   checkTab(type: number) {
     switch (type) {
-      case -1:
-        this.tab = 'ALL';
+      case this.TABS.ALL.value:
+        this.tab = this.TABS.ALL.name;
         break;
-      case 0:
-        this.tab = 'TRANSACTION';
+      case this.TABS.DAILY.value:
+        this.tab = this.TABS.DAILY.name;
         break;
-      case 1:
-        this.tab = 'ANNUAL';
+      case this.TABS.YEARLY.value:
+        this.tab = this.TABS.YEARLY.name;
         break;
-      // case 2:
-      //   this.tab = 'CAMPAIGN';
-      //   break;
     }
+  }
+
+  setDate(duration: string) {
+    this.searchForm.controls.toDate.setValue(moment((new Date()).toDateString()).format('DD/MM/YYYY'));
+    switch (duration) {
+      case this.DURATION.DAY:
+        this.searchForm.controls.fromDate.setValue(moment(new Date()).add(-7, 'days').format('DD/MM/YYYY'));
+        break;
+      case this.DURATION.MONTH:
+        this.searchForm.controls.fromDate.setValue(moment(new Date()).add(-30, 'days').format('DD/MM/YYYY'));
+        break;
+      case this.DURATION.YEAR:
+        this.searchForm.controls.fromDate.setValue(moment(new Date()).add(-1, 'years').format('DD/MM/YYYY'));
+        break;
+    }
+  }
+
+  formatDate(date: string) {
+    if (date) {
+      return date.split('/')[0] + '-' + date.split('/')[1] + '-' + date.split('/')[2];
+    }
+    return null;
   }
 }
