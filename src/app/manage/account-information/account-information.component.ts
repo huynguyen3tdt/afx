d8328623d33771ea2593fb258b4d41dd666e1e5b
haonguyen <haonguyen@tamdongtam.vnnode>
2020-02-26 14:34:14 +0700

    get api corporation myaccount

--- src/app/manage/account-information/account-information.component.ts
+++ src/app/manage/account-information/account-information.component.ts
@@ -3,7 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { WithdrawRequestService } from 'src/app/core/services/withdraw-request.service';
 import { Mt5Model, WithdrawAmountModel } from 'src/app/core/model/withdraw-request-response.model';
 import { UserService } from './../../core/services/user.service';
-import { UserModel } from 'src/app/core/model/user.model';
+import { UserModel, CorporateResponse, CorporateModel } from 'src/app/core/model/user.model';
 import { FormGroup, FormControl } from '@angular/forms';
 import { requiredInput } from 'src/app/core/helper/custom-validate.helper';
 
@@ -26,40 +26,81 @@ export class AccountInformationComponent implements OnInit {
   editAddress: boolean;
   editEmail: boolean;
   editPhone: boolean;
+  editCorAddress: boolean;
+  editCorPhone: boolean;
+  editCorFax: boolean;
+  editPersonBod: boolean;
+  editPersonPic: boolean;
+  editPersonPicname: boolean;
+  editPersonPhone: boolean;
+  editPersonEmail: boolean;
   // editLanguage: boolean;
   userInfor: UserModel;
+  corporateInfor: CorporateModel;
   userForm: FormGroup;
+  corporateForm: FormGroup;
   countries = ['Vietnamese', 'English'];
   postcode: any;
   isSubmitted: boolean;
   prefecture: any;
   county: any;
   showSave = false;
+  showSaveCor = false;
   typeUser: string;
-  test;
+  corPrefecture: any;
+  corDistrict: any;
+  test: any;
+
 
   ngOnInit() {
     this.initUserForm();
+    this.initCorporateForm();
     this.editAddress = false;
     this.editEmail = false;
     this.editPhone = false;
+    this.editCorAddress = false;
+    this.editCorPhone = false;
+    this.editCorFax = false;
+    this.editPersonBod = false;
+    this.editPersonPic = false;
+    this.editPersonPicname = false;
+    this.editPersonPhone = false;
+    this.editPersonEmail = false;
     // this.editLanguage = false;
     this.getMt5Infor();
     this.getWithDrawAmount();
+    this.getCorporateInfor();
   }
 
   initUserForm() {
     this.userForm = new FormGroup({
-      postCode: new FormControl('', requiredInput),
+      postCode: new FormControl(),
       searchPrefe: new FormControl(),
       searchCountry: new FormControl(),
-      house_numb: new FormControl('', requiredInput),
-      email: new FormControl('', requiredInput),
-      phone: new FormControl('', requiredInput),
+      house_numb: new FormControl(),
+      email: new FormControl(),
+      phone: new FormControl(),
       // language: new FormControl(),
     });
   }
-
+  initCorporateForm() {
+    this.corporateForm = new FormGroup({
+      cor_postcode: new FormControl(),
+      cor_prefec: new FormControl(),
+      cor_district: new FormControl(),
+      cor_house: new FormControl(),
+      cor_build: new FormControl(),
+      cor_phone: new FormControl(),
+      cor_fax: new FormControl(),
+      person_bod: new FormControl(),
+      person_pic: new FormControl(),
+      per_picname: new FormControl(),
+      person_picname2: new FormControl(),
+      // person_gender: new FormControl(),
+      person_phone: new FormControl(),
+      person_email: new FormControl(),
+    });
+  }
   changeLang(event) {
     this.translateee.use(event);
   }
@@ -75,7 +116,7 @@ export class AccountInformationComponent implements OnInit {
         this.userForm.controls.searchCountry.setValue(this.userInfor.address.value.street);
         this.userForm.controls.house_numb.setValue(this.userInfor.address.value.street2);
         this.userForm.controls.email.setValue(this.userInfor.email.value);
-        this.userForm.controls.phone.setValue(this.userInfor.phone);
+        this.userForm.controls.phone.setValue(this.userInfor.mobile);
         // this.userForm.controls.language.setValue(this.userInfor.lang);
         // this.postcode = this.userInfor.postcode.value;
         console.log('userInfooo ', this.userInfor);
@@ -84,6 +125,29 @@ export class AccountInformationComponent implements OnInit {
     });
   }
 
+  getCorporateInfor() {
+    this.userService.getCorporateInfor().subscribe(response => {
+      if (response.meta.code === 200) {
+        this.corporateInfor = response.data;
+        this.corPrefecture = this.corporateInfor.corporation.address.value.city;
+        this.corDistrict = this.corporateInfor.corporation.address.value.street;
+        console.log('corInfooo ', this.corporateInfor);
+        this.corporateForm.controls.cor_postcode.setValue(this.corporateInfor.corporation.zip.value);
+        // this.corporateForm.controls.cor_prefec.setValue(this.corporateInfor.corporation.address.value.city);
+        // this.corporateForm.controls.cor_district.setValue(this.corporateInfor.corporation.address.value.street);
+        this.corporateForm.controls.cor_house.setValue(this.corporateInfor.corporation.address.value.street2);
+        this.corporateForm.controls.cor_build.setValue(this.corporateInfor.corporation.address.value.fx_street3);
+        this.corporateForm.controls.cor_phone.setValue(this.corporateInfor.corporation.mobile);
+        this.corporateForm.controls.cor_fax.setValue(this.corporateInfor.corporation.fx_fax.value);
+        this.corporateForm.controls.person_bod.setValue(this.corporateInfor.pic.fx_dept);
+        this.corporateForm.controls.person_pic.setValue(this.corporateInfor.pic.function);
+        this.corporateForm.controls.per_picname.setValue(this.corporateInfor.pic.name);
+        this.corporateForm.controls.per_picname2.setValue(this.corporateInfor.pic.fx_name1);
+        this.corporateForm.controls.person_phone.setValue(this.corporateInfor.pic.mobile);
+        this.corporateForm.controls.person_email.setValue(this.corporateInfor.pic.email.value);
+      }
+    });
+  }
   getMt5Infor() {
     this.withdrawRequestService.getmt5Infor().subscribe(response => {
       if (response.meta.code === 200) {
@@ -150,9 +214,6 @@ export class AccountInformationComponent implements OnInit {
       case 'phone':
         this.editPhone = true;
         break;
-      // case 'lang':
-      //   this.editLanguage = true;
-      //   break;
     }
   }
 
@@ -168,12 +229,65 @@ export class AccountInformationComponent implements OnInit {
       case 'phone':
         this.editPhone = false;
         break;
-      // case 'lang':
-      //   this.editLanguage = false;
-      //   break;
-
     }
 
   }
-
+  showEditFieldCor(field: string) {
+    this.showSaveCor = true;
+    switch (field) {
+      case 'cor-address':
+        this.editCorAddress = true;
+        break;
+      case 'cor-phone':
+        this.editCorPhone = true;
+        break;
+      case 'cor-fax':
+        this.editCorFax = true;
+        break;
+      case 'department':
+        this.editPersonBod = true;
+        break;
+      case 'pic-position':
+        this.editPersonPic = true;
+        break;
+      case 'pic-name':
+        this.editPersonPicname = true;
+        break;
+      case 'p-phone':
+        this.editPersonPhone = true;
+        break;
+      case 'p-email':
+        this.editPersonEmail = true;
+        break;
+    }
+  }
+  cancelEditCor(field: string) {
+    this.showSaveCor = false;
+    switch (field) {
+      case 'cor-address':
+        this.editCorAddress = false;
+        break;
+      case 'cor-phone':
+        this.editCorPhone = false;
+        break;
+      case 'cor-fax':
+        this.editCorFax = false;
+        break;
+      case 'department':
+        this.editPersonBod = false;
+        break;
+      case 'pic-position':
+        this.editPersonPic = false;
+        break;
+      case 'pic-name':
+        this.editPersonPicname = false;
+        break;
+      case 'p-phone':
+        this.editPersonPhone = false;
+        break;
+      case 'p-email':
+        this.editPersonEmail = false;
+        break;
+    }
+  }
 }
