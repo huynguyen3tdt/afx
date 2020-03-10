aa02dc8f5b6e4f38186fea4ed12a85948f08f50a
MinhPhuongNguyenCNTT <46044208+MinhPhuongNguyenCNTT@users.noreply.github.com>
2020-03-10 16:32:22 +0700

    update notification

--- src/app/manage/notifications/notifications.component.ts
+++ src/app/manage/notifications/notifications.component.ts
@@ -53,10 +53,7 @@ export class NotificationsComponent implements OnInit {
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.unreadAll = false;
-    this.unreadCampagn = false;
-    this.unreadImportant = false;
-    this.unreadNotification = false;
+    this.initFilterRead();
     this.currentPage = 1;
     this.pageSize = 10;
     this.checkAgreement = false;
@@ -206,6 +203,48 @@ export class NotificationsComponent implements OnInit {
     }
   }
 
+  changeTab(type: number) {
+    this.pageSize = 10;
+    this.initFilterRead();
+    switch (type) {
+      case this.TABS.ALL.value:
+        this.tab = this.TABS.ALL.name;
+        this.getListNotifications(this.pageSize, this.currentPage, this.unreadAll, this.TABS.ALL.value);
+        break;
+      case this.TABS.IMPORTANT.value:
+        this.tab = this.TABS.IMPORTANT.name;
+        this.getListNotifications(this.pageSize, this.currentPage, this.unreadImportant, this.TABS.IMPORTANT.value);
+        break;
+      case this.TABS.NOTIFICATIONS.value:
+        this.tab = this.TABS.NOTIFICATIONS.name;
+        this.getListNotifications(this.pageSize, this.currentPage, this.unreadNotification, this.TABS.NOTIFICATIONS.value);
+        break;
+      case this.TABS.CAMPAIGN.value:
+        this.tab = this.TABS.CAMPAIGN.name;
+        this.getListNotifications(this.pageSize, this.currentPage, this.unreadCampagn, this.TABS.CAMPAIGN.value);
+        break;
+    }
+  }
+
+  changeTotalItem(event) {
+    this.pageSize = event.target.value;
+    this.currentPage = 1;
+    switch (this.tab) {
+      case this.TABS.ALL.name:
+        this.getListNotifications(this.pageSize, this.currentPage, this.unreadAll, this.TABS.ALL.value);
+        break;
+      case this.TABS.IMPORTANT.name:
+        this.getListNotifications(this.pageSize, this.currentPage, this.unreadImportant, this.TABS.IMPORTANT.value);
+        break;
+      case this.TABS.NOTIFICATIONS.name:
+        this.getListNotifications(this.pageSize, this.currentPage, this.unreadNotification, this.TABS.NOTIFICATIONS.value);
+        break;
+      case this.TABS.CAMPAIGN.name:
+        this.getListNotifications(this.pageSize, this.currentPage, this.unreadCampagn, this.TABS.CAMPAIGN.value);
+        break;
+    }
+  }
+
   showDetail(index: number, item: Notification) {
     if (item.agreement_flg === 1) {
       this.contentAgeement = item.news_content;
@@ -240,6 +279,13 @@ export class NotificationsComponent implements OnInit {
 
   showimportant() {
     $('.test').toggleClass('opened');
+  }
+
+  initFilterRead() {
+    this.unreadAll = false;
+    this.unreadCampagn = false;
+    this.unreadImportant = false;
+    this.unreadNotification = false;
   }
 
 }
