29af104d1eb70e049754b2412736827ae591e217
haonguyen <haonguyen@tamdongtam.vnnode>
2020-03-23 15:22:45 +0700

    fix login success

--- src/app/register/login/login.component.ts
+++ src/app/register/login/login.component.ts
@@ -108,8 +108,12 @@ export class LoginComponent implements OnInit, AfterViewInit {
         localStorage.setItem(TOKEN_AFX, response.data.access_token);
         localStorage.setItem(ACCOUNT_IDS, JSON.stringify(response.data.account_ids));
         localStorage.setItem(IS_COMPANY, response.data.is_company.toString());
+        if (response.data.module_funding_min_deposit) {
         localStorage.setItem(MIN_DEPOST, response.data.module_funding_min_deposit.toString());
-        localStorage.setItem(MIN_WITHDRAW, response.data.module_funding_min_withdraw.toString());
+        }
+        if (response.data.module_funding_min_withdraw) {
+          localStorage.setItem(MIN_WITHDRAW, response.data.module_funding_min_withdraw.toString());
+        }
         localStorage.setItem(FIRST_LOGIN, '1');
         if (response.data.pwd_change_flg === false) {
           this.router.navigate(['/manage/notifications'], {
