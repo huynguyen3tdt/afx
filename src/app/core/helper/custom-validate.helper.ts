417f76108e048fb97f838414ee18744d71add27b
Nguyễn Minh Hiếu <hieunguyen@tamdongtam.vn>
2020-06-05 10:56:22 +0700

    update maxlength for field and rewrite validate for email

--- src/app/core/helper/custom-validate.helper.ts
+++ src/app/core/helper/custom-validate.helper.ts
@@ -94,7 +94,7 @@ const NOT_SPECIAL_CHARACTERS_FOR_EMAIL = '^[a-zA-Z0-9-._ ]*$';
 const NOT_SPECIAL_CHARACTERS = '^[a-zA-Z0-9 ]*$';
 // /[a-z0-9\._%+!$&*=^|~#%'`?{}/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,16})/ /^[0-9,.]*$/
 const NOT_SPECIAL_CHARACTERS_CONTAIN_DOT = '^[a-zA-Z0-9,./_ ]*$';
-const EMAIL_PATTERN = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const EMAIL_PATTERN = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
 const HALF_PATTERN = /^[ｦ-ﾟ ､0-9a-zA-Z]*$/;
 const FULL_SIZE_NUMBER = /([０-９])/;
 const HALF_SIZE_NUMBER = /[0-9]/;
@@ -104,25 +104,34 @@ const FULLSIZE_HIRAGANA = /[\u3040-\u309f]/;
 const JP_REQUIRED = /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/;
 
 
+// export function emailValidation(control: AbstractControl) {
+//     if (!control.value || typeof control.value === 'string' && !control.value.trim()) {
+//       return DEFAULT_INVALID_REQUIRED;
+//     }
+//     const str1 = control.value.split('@')[0];
+//     const pattern = new RegExp(EMAIL_PATTERN);
+//     const pattern2 = new RegExp(NOT_SPECIAL_CHARACTERS_FOR_EMAIL);
+//     if (!control.value || typeof control.value === 'string' && !control.value.trim()) {
+//       return DEFAULT_INVALID_REQUIRED;
+//     }
+//     if (pattern2.test(str1) === false) {
+//       return DEFAULT_INVALID_EMAIL;
+//     }
+//     if (pattern.test(control.value.trim()) === false) {
+//       return DEFAULT_INVALID_EMAIL;
+//     }
+//     return;
+// }
 export function emailValidation(control: AbstractControl) {
-  if (!control.value || typeof control.value === 'string' && !control.value.trim()) {
-    return DEFAULT_INVALID_REQUIRED;
-  }
-  const str1 = control.value.split('@')[0];
   const pattern = new RegExp(EMAIL_PATTERN);
-  const pattern2 = new RegExp(NOT_SPECIAL_CHARACTERS_FOR_EMAIL);
   if (!control.value || typeof control.value === 'string' && !control.value.trim()) {
     return DEFAULT_INVALID_REQUIRED;
-  }
-  if (pattern2.test(str1) === false) {
-    return DEFAULT_INVALID_EMAIL;
   }
   if (pattern.test(control.value.trim()) === false) {
     return DEFAULT_INVALID_EMAIL;
   }
   return;
 }
-
 
 export function requiredInput(control: AbstractControl) {
   if (!control.value || typeof control.value === 'string' && !control.value.trim()) {
