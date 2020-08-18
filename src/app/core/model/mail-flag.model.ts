8adfb29cef27185a1a8b1299b7c9d674666437d8
ducanh <anhnguyen1@tamdongtam.vn>
2020-08-18 12:00:06 +0700

    Fix setting user

--- src/app/core/model/mail-flag.model.ts
+++ src/app/core/model/mail-flag.model.ts
@@ -8,4 +8,12 @@ export interface MailFlagResponseModel {
 export interface MailFlagModel {
     losscut_email_flg: boolean;
     margincall_email_flg: boolean;
+    company_losscut_flg: boolean;
+    company_margincall_flg: boolean;
+
+}
+
+export interface MailFlagParamModel {
+    losscut_email_flg: boolean;
+    margincall_email_flg: boolean;
 }
