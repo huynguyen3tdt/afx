b32b2a6b34f270b84c69e985c4b96b3952ff3f2d
Nguyễn Minh Hiếu <hieunguyen@tamdongtam.vn>
2020-10-03 09:01:32 +0700

    convert number to string

--- src/app/core/services/global.service.ts
+++ src/app/core/services/global.service.ts
@@ -1,19 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AccountType } from '../model/report-response.model';
 import { TYPEOFTRANHISTORY, PAYMENTMETHOD, TRADING_TYPE } from '../constant/payment-method-constant';
-import { LOCALE } from '../constant/authen-constant';
+import { LOCALE, ACCOUNT_TYPE } from '../constant/authen-constant';
 import { LANGUAGLE } from '../constant/language-constant';
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 import { FX_IMAGE, ICFD_IMAGE, CCFD_IMAGE } from '../constant/img-constant';
 
-export const ACCOUNT_TYPE = {
-  ACCOUNT_FX: { account_type: 1, name: 'FX' },
-  ACCOUNT_CFDIndex: { account_type: 2, name: 'CFD Index' },
-  ACCOUNT_CFDCom: { account_type: 3, name: 'CFD Index' },
-};
 const numeral = require('numeral');
-
 @Injectable({
   providedIn: 'root'
 })
@@ -199,7 +193,7 @@ export class GlobalService {
         }
         const dataObj: AccountType = {
           account_type: element.account_type,
-          account_id: element.trading_account_id,
+          account_id: element.trading_account_id.toString(),
           value : element.value,
           currency: element.currency_cd,
           img_type_account: element.img_type_account
