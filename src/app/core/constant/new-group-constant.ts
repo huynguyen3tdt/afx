793748f335d2517cf8858c6f70659fca51ab0c93
ducanh <anhnguyen1@tamdongtam.vn>
2021-01-05 11:33:37 +0700

    Update news for b2b

--- src/app/core/constant/new-group-constant.ts
+++ src/app/core/constant/new-group-constant.ts
@@ -1,4 +1,8 @@
 export const FX_GROUP = 'IT_FX';
 export const I_CFD_GROUP = 'IT_CFDI';
 export const C_CFD_GROUP = 'IT_CFDC';
-export const B2B_GROUP = 'B2B';
+export const B2B_FX_GROUP = 'B2B_FX';
+export const I_CFD_B2B_GROUP = 'B2B_CFDI';
+export const C_CFD_B2B_GROUP = 'B2B_CFDC';
+
+
